39717dd1514d06e886ce39e53fb65b98df5a1858
Juan Montoya <juan.montoya110@udea.edu.co>
2023-02-21 22:37:32 -0500

    Tanteo con el tipeo de c++

---
+++ inicio-c++/TiposDeDatos.cpp
@@ -0,0 +1,12 @@
+//Tipos de datos en c++, en c++ el tipado es fuerte
+#include <iostream>
+using namespace std;
+int main()
+{
+    int numero=15;
+    float dec=10.3;
+    double mayor=16.5323454;
+    char text='h';
+    cout<<"Este es un numero decimal grande:"<<mayor<<", Este es un texto:"<<text<<", Este es un numero entero:"<<numero<<", Este es un decimal pequeñp:"<<dec<<endl;
+    return 0;
+}
